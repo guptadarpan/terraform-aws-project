a88fd0c6ebdfb812bb267060a0d7cd5fe08e0dd2
guptadarpan <75995226+guptadarpan@users.noreply.github.com>
2026-03-15 23:54:14 +0530

    Create variables.tf

---
+++ environments/staging/variables.tf
@@ -0,0 +1,10 @@
+variable "env_name"              { type = string }
+variable "vpc_cidr"              { type = string }
+variable "public_subnet_cidr"    { type = string }
+variable "private_subnet_cidr"   { type = string }
+variable "private_subnet_cidr_2" { type = string }
+variable "my_ip"                 { type = string }
+variable "db_username"           { type = string }
+variable "db_password"           { type = string }
+variable "key_name"              { type = string }
+variable "ami_id"                { type = string }
