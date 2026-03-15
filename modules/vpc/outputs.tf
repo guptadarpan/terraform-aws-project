5122f40a34baf72a346af91c0d560080208aeb99
guptadarpan <75995226+guptadarpan@users.noreply.github.com>
2026-03-15 17:33:28 +0530

    Create outputs.tf

---
+++ modules/vpc/outputs.tf
@@ -0,0 +1,24 @@
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = aws_vpc.main.id
+}
+
+output "public_subnet_id" {
+  description = "The ID of the public subnet"
+  value       = aws_subnet.public.id
+}
+
+output "private_subnet_id" {
+  description = "The ID of the private subnet"
+  value       = aws_subnet.private.id
+}
+
+output "bastion_sg_id" {
+  description = "The ID of the bastion security group"
+  value       = aws_security_group.bastion_sg.id
+}
+
+output "rds_sg_id" {
+  description = "The ID of the RDS security group"
+  value       = aws_security_group.rds_sg.id
+}
