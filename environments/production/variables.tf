5c5bcb8ff147e16576aca74998ca4e3b2fc69caf
guptadarpan <75995226+guptadarpan@users.noreply.github.com>
2026-03-16 00:01:44 +0530

    Update variables.tf

--- environments/production/variables.tf
+++ environments/production/variables.tf
@@ -1,10 +1,62 @@
-variable "env_name"              { type = string }
-variable "vpc_cidr"              { type = string }
-variable "public_subnet_cidr"    { type = string }
-variable "private_subnet_cidr"   { type = string }
-variable "private_subnet_cidr_2" { type = string }
-variable "my_ip"                 { type = string }
-variable "db_username"           { type = string }
-variable "db_password"           { type = string }
-variable "key_name"              { type = string }
-variable "ami_id"                { type = string }
+variable "env_name" {
+  description = "Name of the environment"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "IP range for the whole VPC"
+  type        = string
+}
+
+variable "public_subnet_cidr" {
+  description = "IP range for the public subnet"
+  type        = string
+}
+
+variable "private_subnet_cidr" {
+  description = "IP range for the first private subnet"
+  type        = string
+}
+
+variable "private_subnet_cidr_2" {
+  description = "IP range for the second private subnet"
+  type        = string
+}
+
+variable "availability_zone" {
+  description = "Primary availability zone"
+  type        = string
+  default     = "us-east-1a"
+}
+
+variable "availability_zone_2" {
+  description = "Secondary availability zone for RDS"
+  type        = string
+  default     = "us-east-1b"
+}
+
+variable "ami_id" {
+  description = "Amazon Machine Image ID for the bastion EC2 instance"
+  type        = string
+}
+
+variable "key_name" {
+  description = "Name of the SSH key pair in AWS"
+  type        = string
+}
+
+variable "my_ip" {
+  description = "Your public IP address"
+  type        = string
+}
+
+variable "db_username" {
+  description = "Master username for the Postgres database"
+  type        = string
+}
+
+variable "db_password" {
+  description = "Master password for the Postgres database"
+  type        = string
+  sensitive   = true
+}
