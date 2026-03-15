17db5d7ca4cc49317d939d694d9ba1046400a864
guptadarpan <75995226+guptadarpan@users.noreply.github.com>
2026-03-15 17:15:32 +0530

    Add Terraform configuration for staging environment

---
+++ environments/staging/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "vpc" {
+  source = "../../modules/vpc"   
+
+  env_name            = var.env_name
+  vpc_cidr            = var.vpc_cidr
+  public_subnet_cidr  = var.public_subnet_cidr
+  private_subnet_cidr = var.private_subnet_cidr
+  my_ip               = var.my_ip
+}
